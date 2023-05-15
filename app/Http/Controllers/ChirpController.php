a8aff07d0346468962b02b9dce1345fd915159a7
mazacon <mazacon@protonmail.com>
2023-05-14 21:43:38 -0500

    implement /chirps PUT logic

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -55,15 +55,23 @@ class ChirpController extends Controller
      */
     public function edit(Chirp $chirp)
     {
-        //
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        $this->authorize('update', $chirp);
+ 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
